1873eec2a6d0d6ebea80374eff20a384170ddb97
Frendzlu <mateuszfrancik.mtf@gmail.com>
2025-12-29 19:21:26 +0100

    reindent with tabs

---
+++ bridge_tc_library/structure/movements/abstract_rotation.py
@@ -0,0 +1,47 @@
+from typing import List, Optional, Any, Tuple, Dict
+from abc import ABC, abstractmethod
+from .strategy import MovementStrategy
+from bridge_tc_library.structure.core import Position
+
+
+class AbstractRotation(ABC):
+	num_tables: int
+	"""
+	Abstract base class for rotation calculators.
+	"""
+
+	def __init__(self):
+		self._pair_rounds_cache: Optional[List[Dict[Tuple[int, str], Any]]] = None
+
+	@abstractmethod
+	def check_if_can_handle(self, num_pairs: int, min_boards_amount: int, max_boards_amount: int) -> bool:
+		"""
+		Checks if the current rotation can handle the given number of pairs and boardgroups.
+		"""
+
+	@abstractmethod
+	def generate_possibile_rotations_draft(self, num_pairs: int, min_boards_amount: int, max_boards_amount: int) -> List[Tuple[int, int, int]]:
+		"""
+		Generates possible rotation drafts.
+		This will generate a list of possible rotations in a draft format.
+		List of tuples: (Amount of rounds: int, Amount of boards: int, amount of boardgroup_sets: int)
+		"""
+
+	@abstractmethod
+	def generate_strategy_for_rotation(self, num_pairs: int, rounds: int, boardgroup_sets: int) -> MovementStrategy:
+		"""
+		Generates movement strategy for the given rotation parameters.
+		"""
+
+	def check_if_bye_needed(self, num_pairs: int) -> bool:
+		"""
+		Indicates if bye is needed in the rotation.
+		If so, one pair will be added to the total number of pairs, so it is even.
+		"""
+		if num_pairs % 2 == 1:
+			self.num_pairs += 1
+			return True
+		else:
+			return False
+
+	
